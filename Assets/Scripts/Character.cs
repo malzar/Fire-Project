d72dca8e9d1a7182055be24096dba91123309977
malzar <noquieroleer@gmail.com>
2017-07-29 14:03:23 +0200

    Character movement
    The chareacter is alive. It's move arround the scene. When detect a wall rotate in under clock wise.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Character : MonoBehaviour {
+
+    public Vector3 _Direcction;
+    public float _Speed;
+    public int _DirectionRotation;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        transform.position += _Direcction * _Speed;
 		
 	}
+
+    void SetDirection(Vector3 Direction){
+        _Direcction = Direction;
+    }
+
+    void OnTriggerEnter(Collider other){
+        //posible caos si metemos random para direccion
+        _DirectionRotation += 90;
+        _DirectionRotation = Mathf.Abs(_DirectionRotation) % 360;
+        _Direcction = new Vector3(Mathf.Cos(Mathf.Deg2Rad * _DirectionRotation), 0, Mathf.Cos(Mathf.Deg2Rad * (_DirectionRotation - 90)));
+        transform.Rotate(Vector3.up, -90);
+    }
 }
